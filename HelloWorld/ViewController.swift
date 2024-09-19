a94c1d2a09e840d2f01b2622a2c907999ccc1e45
ruslanarystanov <r.arystanov@arbuz.kz>
2024-09-19 16:57:51 +0500

    twoHomeWorkUiCit

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLens: UIView!
+    @IBOutlet var yellowLens: UIView!
+    @IBOutlet var greenLens: UIView!
+    @IBOutlet var nextButton: UIButton!
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        nextButton.layer.cornerRadius = 20
+        redLens.layer.cornerRadius = 30
+        yellowLens.layer.cornerRadius = 30
+        greenLens.layer.cornerRadius = 30
+        redLens.alpha = 0.3
+        yellowLens.alpha = 0.3
+        greenLens.alpha = 0.3
     }
 
-
+    @IBAction func colorChange() {
+        counter += 1
+        
+        switch counter {
+        case 1:
+            greenLens.alpha = 0.3
+            redLens.alpha = 1
+        case 2:
+            redLens.alpha = 0.3
+            yellowLens.alpha = 1
+        case 3:
+            yellowLens.alpha = 0.3
+            greenLens.alpha = 1
+            counter = 0
+        default:
+            counter = 0
+        }
+    }
 }
 
